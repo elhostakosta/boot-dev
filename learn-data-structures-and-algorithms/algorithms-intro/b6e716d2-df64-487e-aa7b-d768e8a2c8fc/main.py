bcf66f86fc0be2ce8675efba3376d8a5c085e11b
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-03-29 06:10:43 +0200

    Add b6e716d2-df64-487e-aa7b-d768e8a2c8fc solution

---
+++ learn-data-structures-and-algorithms/algorithms-intro/b6e716d2-df64-487e-aa7b-d768e8a2c8fc/main.py
@@ -0,0 +1,11 @@
+def find_minimum(nums):
+    minimum = float("inf")
+
+    if (len(nums) == 0):
+        return None
+
+    for num in nums:
+        if num < minimum:
+            minimum = num
+
+    return minimum
